c9734c7b2a12afc6acdc46985f5364d39573ece6
drana123 <dhirajrana395@gmail.com>
2021-08-25 01:01:41 +0530

    done changes

---
+++ CreditCardAPI/CreditCardEmiCalculator/CreditCardLoan.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace CreditCardEmiCalculator
+{
+    public class CreditCardLoan
+    {
+        int amount, tenure;
+        double roi;
+
+        public CreditCardLoan(int amount, int tenure, double roi)
+        {
+            this.amount = amount;
+            this.tenure = tenure;
+            this.roi = roi;
+        }
+
+        public int FindEmi()
+        {
+            EmiCalculator _EmiCalculator = new EmiCalculator();
+            // Console.WriteLine("The emi is {0} per month.", _EmiCalculator.CalculateEmi(amount, tenure, roi));
+            return _EmiCalculator.CalculateEmi(amount, tenure, roi);
+        }
+        public int FindTotalInterest()
+        {
+            TotalInterestCalculator _TotalInterest = new TotalInterestCalculator();
+            //Console.WriteLine("The total interest is {0}.", _TotalInterest.CalculateTotalInterest(amount, tenure, roi));
+            return _TotalInterest.CalculateTotalInterest(amount, tenure, roi);
+        }
+        public int FindTotaAmount()
+        {
+            NetPayableCalculator _NetPayableCalculator = new NetPayableCalculator();
+            // Console.WriteLine("The total amount is {0}.", _NetPayableCalculator.CalculateNetAmount(amount, tenure, roi));
+            return _NetPayableCalculator.CalculateNetAmount(amount, tenure, roi);
+
+        }
+
+    }
+}
---
+++ CreditCardAPI/CreditCardEmiCalculatorAPI/Controllers/EmiCalculatorController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CreditCardEmiCalculator;
+
+namespace CreditCardEmiCalculatorAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EMICalculatorController : ControllerBase
+    {
+
+        [HttpGet("{amount}/{tenure}/{rateOfInterest}")]
+        public string EMICalculatorClass(int amount, int tenure, float rateOfInterest)
+        {
+            CreditCardLoan _CreditCardLoan = new CreditCardLoan(amount, tenure, rateOfInterest);
+
+            int emi = _CreditCardLoan.FindEmi();
+            int interest = _CreditCardLoan.FindTotalInterest();
+            int netAmount = _CreditCardLoan.FindTotaAmount();
+
+            string resultDisplay = $"The emi is:{emi}. The interest is:{interest}. The total amount is:{netAmount}.";
+
+            return resultDisplay;
+        }
+
+    }
+}
