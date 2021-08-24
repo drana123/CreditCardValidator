63049bafe4aa44f017e060ba74b836a7151b15cb
drana123 <dhirajrana395@gmail.com>
2021-08-25 00:58:58 +0530

    files added

---
+++ CreditCardValidator/CheckCreditCardNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using CreditCardValidatorLib;
+
+namespace CheckCreditCardNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Credit Card Number");
+            string CC_Number = Console.ReadLine();
+            CreditCardValidator CheckValidity= new CreditCardValidator();
+            try
+            {
+                CreditCardValidator.validate(CC_Number);
+                Console.WriteLine("Valid Card Number");
+
+            }
+            catch ( Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
---
+++ CreditCardValidator/CreditCardValidatorLib/CreditCardValidator.cs
@@ -0,0 +1,73 @@
+﻿
+using System;
+
+namespace CreditCardValidatorLib
+{
+    public class CreditCardValidator
+    {
+
+        public static bool validate(string CreditCardNumber)
+        {
+            //Validate length
+            int CreditCardNumberLength = CreditCardNumber.Length;
+            if (CreditCardNumberLength != 13 && CreditCardNumberLength != 16) {
+                throw new ArgumentException("Given CC Number Length is Invalid");
+            }
+
+            //Validate chars
+            for(int i = 0; i < CreditCardNumberLength; i++)
+            {
+                if(!(CreditCardNumber[i] >='0' && CreditCardNumber[i] <= '9'))
+                {
+                    throw new ArgumentException("Given CC number contains Invalid Character");
+                }
+            }
+
+            //Validate IIN 
+            string IIN="";
+            IIN += CreditCardNumber[0];
+            if (IIN != "4")
+            {
+                IIN += CreditCardNumber[1];
+                int IINumber = Convert.ToInt32(IIN);
+                if(!(IINumber >=51 && IINumber <= 55))
+                {
+                    throw new ArgumentException("Given CC number doesnt belong to Visa/Mastercard");
+
+                }
+
+            }
+            else
+            {
+                if (CreditCardNumberLength == 16)
+                {
+                    throw new ArgumentException("Given CC number doesnt belong to Visa/Mastercard");
+
+                }
+            }
+            //Validate Checksum
+            int Sum = 0;
+            bool isCorrectPosition = false;
+            for (int i = CreditCardNumberLength - 1; i >= 0; i--)
+            {
+
+                int digit = CreditCardNumber[i] - '0';
+
+                if (isCorrectPosition == true)
+                    digit = digit * 2;
+
+                Sum += digit / 10;
+                Sum += digit % 10;
+
+                isCorrectPosition = !isCorrectPosition;
+            }
+            if (Sum % 10!= 0)
+            {
+                throw new ArgumentException("Given CC number failed to pass Luhn Algorithm");
+
+            }
+            //Valid card number
+            return true;
+        }
+    }
+}
